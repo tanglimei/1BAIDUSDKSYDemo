aa4ba9850c07b7c0657dab14e1447ae312cadd28
tanglimei <562630424@qq.com>
2015-12-18 15:20:42 +0800

    First Commit
    第一次提交项目

---
+++ SYSDK/BAIDUSDK/QuickUnifyPlatformDemo/build/QuickUnifyPlatformDemo.build/Release-iphoneos/QuickUnifyPlatformDemo.build/Objects-normal/armv7/AppDelegate.d
@@ -0,0 +1,8 @@
+dependencies: \
+  /Users/KuaiYong/Desktop/QuickUnify/Quick/QuickSDK/Common/Source/Demo/AppDelegate.m \
+  /Users/KuaiYong/Desktop/QuickUnify/Quick/QuickSDK/Common/Source/Demo/AppDelegate.h \
+  /Users/KuaiYong/Desktop/QuickUnify/Quick/QuickSDK/SYSDK/BAIDUSDK/QuickUnifyPlatformDemo/../../../Common/SDK/Platform/BAIDUSDK/SYFramework/QuickUnifyPlatform.framework/Headers/QuickPlatformName.h \
+  /Users/KuaiYong/Desktop/QuickUnify/Quick/QuickSDK/SYSDK/BAIDUSDK/QuickUnifyPlatformDemo/../../../Common/SDK/Platform/BAIDUSDK/SYFramework/QuickUnifyPlatform.framework/Headers/QuickConstant.h \
+  /Users/KuaiYong/Desktop/QuickUnify/Quick/QuickSDK/SYSDK/BAIDUSDK/QuickUnifyPlatformDemo/../../../Common/SDK/Platform/BAIDUSDK/SYFramework/QuickUnifyPlatform.framework/Headers/QuickProductInfo.h \
+  /Users/KuaiYong/Desktop/QuickUnify/Quick/QuickSDK/SYSDK/BAIDUSDK/QuickUnifyPlatformDemo/../../../Common/SDK/Platform/BAIDUSDK/SYFramework/QuickUnifyPlatform.framework/Headers/QuickUnifyPlatform.h \
+  /Users/KuaiYong/Desktop/QuickUnify/Quick/QuickSDK/SYSDK/BAIDUSDK/QuickUnifyPlatformDemo/../../../Common/SDK/Platform/BAIDUSDK/SYFramework/QuickUnifyPlatform.framework/Modules/module.modulemap
---
+++ SYSDK/BAIDUSDK/QuickUnifyPlatformDemo/build/QuickUnifyPlatformDemo.build/Release-iphoneos/QuickUnifyPlatformDemo.build/Objects-normal/armv7/ViewController.d
@@ -0,0 +1,8 @@
+dependencies: \
+  /Users/KuaiYong/Desktop/QuickUnify/Quick/QuickSDK/Common/Source/Demo/ViewController.m \
+  /Users/KuaiYong/Desktop/QuickUnify/Quick/QuickSDK/Common/Source/Demo/ViewController.h \
+  /Users/KuaiYong/Desktop/QuickUnify/Quick/QuickSDK/SYSDK/BAIDUSDK/QuickUnifyPlatformDemo/../../../Common/SDK/Platform/BAIDUSDK/SYFramework/QuickUnifyPlatform.framework/Headers/QuickPlatformName.h \
+  /Users/KuaiYong/Desktop/QuickUnify/Quick/QuickSDK/SYSDK/BAIDUSDK/QuickUnifyPlatformDemo/../../../Common/SDK/Platform/BAIDUSDK/SYFramework/QuickUnifyPlatform.framework/Headers/QuickConstant.h \
+  /Users/KuaiYong/Desktop/QuickUnify/Quick/QuickSDK/SYSDK/BAIDUSDK/QuickUnifyPlatformDemo/../../../Common/SDK/Platform/BAIDUSDK/SYFramework/QuickUnifyPlatform.framework/Headers/QuickProductInfo.h \
+  /Users/KuaiYong/Desktop/QuickUnify/Quick/QuickSDK/SYSDK/BAIDUSDK/QuickUnifyPlatformDemo/../../../Common/SDK/Platform/BAIDUSDK/SYFramework/QuickUnifyPlatform.framework/Headers/QuickUnifyPlatform.h \
+  /Users/KuaiYong/Desktop/QuickUnify/Quick/QuickSDK/SYSDK/BAIDUSDK/QuickUnifyPlatformDemo/../../../Common/SDK/Platform/BAIDUSDK/SYFramework/QuickUnifyPlatform.framework/Modules/module.modulemap
